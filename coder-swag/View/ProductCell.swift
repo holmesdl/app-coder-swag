305e4c684908efd0bdb3816b60315aea45c6e83d
Darren Holmes <darrenholmes@darrens-mbp.home>
2020-01-27 13:59:21 +0000

    Product controller layout

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Darren Holmes on 27/01/2020.
+//  Copyright © 2020 Darren Holmes. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
