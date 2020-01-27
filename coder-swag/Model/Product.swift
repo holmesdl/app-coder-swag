b6afcaee29fce7350ec0801d36d2cca17a4f4d01
Darren Holmes <darrenholmes@darrens-mbp.home>
2020-01-27 14:36:16 +0000

    product data

--- coder-swag/Model/Product.swift
+++ coder-swag/Model/Product.swift
@@ -1,19 +1,21 @@
 //
-//  Category.swift
+//  Product.swift
 //  coder-swag
 //
-//  Created by Darren Holmes on 24/01/2020.
+//  Created by Darren Holmes on 27/01/2020.
 //  Copyright © 2020 Darren Holmes. All rights reserved.
 //
 
 import Foundation
 
-struct Category {
+struct Product {
     private(set) public var title: String
+    private(set) public var price: String
     private(set) public var imageName: String
     
-    init(title: String, imageName: String) {
+    init(title: String, price: String, imageName: String) {
         self.title = title
+        self.price = price
         self.imageName = imageName
     }
 }
